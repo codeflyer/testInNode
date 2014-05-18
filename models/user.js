3adc250f2393e6b2a7bcfc0a83219279ebbc56ae
Davide Fiorello <davide@codeflyer.com>
2014-05-18 22:38:27 +0200

    Added should e setter to the user model

--- models/user.js
+++ models/user.js
@@ -4,4 +4,16 @@ var User = function User() {
     this.email = '';
 };
 
+User.prototype.setName = function(name) {
+    this.name = name;
+};
+
+User.prototype.setSurname = function(surname) {
+    this.surname = surname;
+};
+
+User.prototype.setEmail = function(email) {
+    this.email = email;
+};
+
 module.exports = User;
--- tests/test.user.js
+++ tests/test.user.js
@@ -1,6 +1,21 @@
+require('should');
 var User = require('../models/user');
 describe('User', function(){
     it('Create instance', function() {
         var user = new User();
+        user.name.should.be.equal('');
+        user.surname.should.be.equal('');
+        user.email.should.be.equal('');
+    });
+
+    it('Set values', function() {
+        var user = new User();
+        user.setName('Davide');
+        user.setSurname('Fiorello');
+        user.setEmail('davide@codeflyer.com');
+
+        user.name.should.be.equal('Davide');
+        user.surname.should.be.equal('Fiorello');
+        user.email.should.be.equal('davide@codeflyer.com');
     });
 });
