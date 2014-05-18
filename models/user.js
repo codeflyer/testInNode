2709d5906657103cb25f4d6849f3654c4b2dfd3e
Davide Fiorello <davide@codeflyer.com>
2014-05-18 22:31:50 +0200

    Test User instance

---
+++ models/user.js
@@ -0,0 +1,7 @@
+var User = function User() {
+    this.name = '';
+    this.surname = '';
+    this.email = '';
+};
+
+module.exports = User;
---
+++ tests/test.user.js
@@ -0,0 +1,6 @@
+var User = require('../models/user');
+describe('User', function(){
+    it('Create instance', function() {
+        var user = new User();
+    });
+});
